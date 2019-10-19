0941119d4664aba4cfb5d58a97ebe3748e1d355c
travis-johnson <travisj@dzone.com>
2019-10-19 19:25:40 -0400

    random page and results now returned

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,11 @@
+
+
+var pages = Math.floor(Math.random()*299);
+console.log(pages);
 var settings = {
     "async": true,
     "crossDomain": true,
-    "url": "https://api.themoviedb.org/3/discover/movie?with_genres=27&primary_release_date.lte=2000-01-01&primary_release_date.gte=1970-01-01&page=1&page2&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=a751d6f146daca63096db49c63690e9f",
+    "url": "https://api.themoviedb.org/3/discover/movie?with_genres=27&primary_release_date.lte=2000-01-01&primary_release_date.gte=1970-01-01&page="+pages+"&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=a751d6f146daca63096db49c63690e9f",
     "method": "GET",
     "headers": {},
     "data": "{}"
