e64f1ab489c8eedbc5f32b2fa82741175fb571c3
travis-johnson <travisj@dzone.com>
2019-10-21 09:27:43 -0400

    added custom font and removed display rules for body/container

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
 
-
-var pages = Math.floor(Math.random()*299);
+// TODO get number of pages from API call and populate variable
+var pages = Math.floor(Math.random()*78);
 console.log(pages);
 var settings = {
     "async": true,
     "crossDomain": true,
-    "url": "https://api.themoviedb.org/3/discover/movie?with_genres=27&without_genres=99,18,10751,10402,10749&primary_release_date.lte=2000-01-01&primary_release_date.gte=1970-01-01&page="+pages+"&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=a751d6f146daca63096db49c63690e9f",
+    "url": "https://api.themoviedb.org/3/discover/movie?with_genres=27&without_genres=99,18,10751,10402,10749&primary_release_date.lte=2019-01-01&primary_release_date.gte=1970-01-01&page="+pages+"&include_video=false&include_adult=false&vote_average.gte=6&region=US&sort_by=popularity.desc&language=en-US&api_key=a751d6f146daca63096db49c63690e9f",
     "method": "GET"
   }
 
@@ -18,8 +18,8 @@ var settings = {
   
 
 $(document).ready(function () {
-    $.ajax(genres).done(function (data){
-        console.log(data);
+    $.ajax(genres).done(function (x){
+        console.log(x);
     });
 
     $.ajax(settings).done(function (response) {
