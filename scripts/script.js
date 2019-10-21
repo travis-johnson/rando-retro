d6bb4bbf354cb49b02b6d8cbfb5f138de9d5643a
travis-johnson <travisj@dzone.com>
2019-10-21 19:35:25 -0400

    refactored index.html.  page now reloads on fail

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
 
 // TODO get number of pages from API call and populate variable
-var pages = Math.floor(Math.random()*78);
+var pages = Math.floor(Math.random()*28);
 console.log(pages);
 var settings = {
     "async": true,
     "crossDomain": true,
-    "url": "https://api.themoviedb.org/3/discover/movie?with_genres=27&without_genres=99,18,10751,10402,10749&primary_release_date.lte=2019-01-01&primary_release_date.gte=1970-01-01&page="+pages+"&include_video=false&include_adult=false&vote_average.gte=6&region=US&sort_by=popularity.desc&language=en-US&api_key=a751d6f146daca63096db49c63690e9f",
+    "url": "https://api.themoviedb.org/3/discover/movie?with_genres=27&without_genres=99,18,10751,10402,10749&primary_release_date.lte=1999-01-01&primary_release_date.gte=1970-01-01&page="+pages+"&include_video=false&include_adult=false&vote_average.gte=6&region=US&sort_by=popularity.desc&language=en-US&api_key=a751d6f146daca63096db49c63690e9f",
     "method": "GET"
   }
 
@@ -30,6 +30,7 @@ $(document).ready(function () {
         $('#poster').attr('src', 'https://image.tmdb.org/t/p/w780'+ data.poster_path)
         $('#title').append(data.title)
         $('#desc > p').append(data.overview);
+      }).fail(function(textStatus) { 
+        location.reload();
       });
 });
-  
