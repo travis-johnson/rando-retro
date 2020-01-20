d840c234a30f88787e79d0e984eb135665cdbe0c
travis-johnson <travisj@dzone.com>
2020-01-19 23:34:09 -0500

    removed cast, replaced with trailer

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,7 @@ console.log(pages);
 var settings = {
     "async": true,
     "crossDomain": true,
-    "url": "https://api.themoviedb.org/3/discover/movie?with_genres=27&without_genres=99,18,10751,10402,10749&primary_release_date.lte=1999-01-01&primary_release_date.gte=1970-01-01&page="+pages+"&include_video=false&include_adult=false&vote_average.gte=6&region=US&sort_by=popularity.desc&language=en-US&api_key=a751d6f146daca63096db49c63690e9f",
+    "url": "https://api.themoviedb.org/3/discover/movie?with_genres=27&without_genres=99,18,10751,10402,10749&primary_release_date.lte=1999-01-01&primary_release_date.gte=1970-01-01&page="+pages+"&include_video=false&include_adult=false&vote_average.gte=5&region=US&sort_by=popularity.desc&language=en-US&api_key=a751d6f146daca63096db49c63690e9f",
     "method": "GET"
   }
 
@@ -36,25 +36,38 @@ $(document).ready(function () {
           "url": "https://api.themoviedb.org/3/movie/"+ movieId +"/credits?api_key=a751d6f146daca63096db49c63690e9f&language=en-US",
           "method": "GET"
         }
-        
-        // var cast = "https://api.themoviedb.org/3/movie/"+ movieId + "/credits?api_key="+ key ;
-        // console.log(cast);
+        function getVideo() {
+          $.ajax({
+            type: 'GET',
+            url: 'https://www.googleapis.com/youtube/v3/search',
+            data: {
+                key: 'AIzaSyBJN8aQr9wK-emVuM0mEx5eKR23ImFk3_0',
+                q: data.title + "trailer",
+                part: 'snippet',
+                maxResults: 1,
+                type: 'video',
+                videoEmbeddable: true,
+            },
+            success: function(data){
+                embedVideo(data)
+            },
+            error: function(response){
+                console.log("Request Failed");
+            }
+          });
+        }
+        function embedVideo(data) {
+          $('iframe').attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId)
+         
+      }
+      getVideo();
         console.log(data);
+        var yr = data.release_date.slice(0,4);
         $('#poster').attr('src', 'https://image.tmdb.org/t/p/w500'+ data.poster_path)
-        $('#poster').attr('alt', data.title)
-        $('#movie_title').append(data.title)
-        $('#desc > p').append(data.overview);
-        $.ajax(credits).done(function tmdb(c) {
-
-          console.log(c);
-            $('#job').append(c.crew[0].job);
-            $('#dir_name').append(c.crew[0].name);
-            $('#dir_img').attr('src', 'https://image.tmdb.org/t/p/w92'+ c.crew[0].profile_path);
-
-            $('#role1').append(c.cast[0].job);
-            $('#a1_name').append(c.cast[0].name);
-            $('#a1_img').attr('src','https://image.tmdb.org/t/p/w92'+ c.cast[0].profile_path);
-        })
+        $('#poster').attr('alt', data.title);
+        $('#movie_title').append(data.title);
+        $('#year').append(yr);
+        $('#plot > p').append(data.overview);
 
       }).fail(function(textStatus) { 
         location.reload();
